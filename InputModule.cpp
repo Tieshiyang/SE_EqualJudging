b9e49ac1f0bdc10c3ab531a01566482364cbc655
Tieshiyang <3175599128@qq.com>
2022-11-10 15:18:28 +0800

    complet_real

--- InputModule.cpp
+++ InputModule.cpp
@@ -6,7 +6,7 @@ using namespace std;
 InputModule::InputModule(string input_path){
     GetFileNames(input_path,oj_names);
     oj_num = oj_names.size();
-    file_nams.resize(oj_num);
+    file_names.resize(oj_num);
     for(int i = 0;i < oj_num; i++){
         system(("rm "+oj_names[i]+"/*.out").c_str());
         //get all .cpp files into file_nams
